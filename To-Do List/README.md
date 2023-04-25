439ab2fd9c6969062a9c9e2c115a53edb75ed6ed
CathleenWM <cathleen5068@gmail.com>
2023-04-25 19:39:58 +0200

    ToDo list app

--- To-Do List/README.md
+++ To-Do List/README.md
@@ -1,4 +1,4 @@
-# To-Do List
+# To-Do list
 
 This template should help get you started developing with Vue 3 in Vite.
 
--- To-Do List/src/App.vue
+++ To-Do List/src/App.vue
@@ -1,47 +1,66 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref, onMounted, computed, watch } from 'vue'
+
+const todos = ref([])
+const input_content = ref('')
+
+const todos_asc = computed(() => todos.value.sort((a,b) =>{
+	  return a.createdAt - b.createdAt
+}))
+
+watch(todos, (newVal) => {
+ 	localStorage.setItem('todos', JSON.stringify(newVal))
+ }, {
+	deep: true
+})
+
+const addTodo = () => {
+	if (input_content.value.trim() === '') {
+		return
+	}
+	todos.value.push({
+		content: input_content.value,
+		done: false,
+		editable: false,
+		createdAt: new Date().getTime()
+	})
+}
+
+onMounted(() => {
+	todos.value = JSON.parse(localStorage.getItem('todos')) || []
+})
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+	<main class="app">
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+		<section class="create-todo">
+			<h3>CREATE A TODO</h3>
 
-  <main>
-    <TheWelcome />
-  </main>
+			<form id="new-todo-form" @submit.prevent="addTodo">
+				<h4>What's on your todo list?</h4>
+				<input 
+					type="text" 
+					placeholder="e.g. make a todo list"
+					v-model="input_content" />
+				
+				<input type="submit" value="Add todo" />
+			</form>
+		</section>
+
+		<section class="todo-list">
+			<h3>Todo list</h3>
+			<div class="list" id="todo-list">
+
+				<div v-for="todo in todos_asc" :class="`todo-item ${todo.done && 'done'}`">
+
+					<div class="todo-content">
+						<input type="text" v-model="todo.content" />
+					</div>
+				</div>
+			</div>
+		</section>
+
+	</main>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
